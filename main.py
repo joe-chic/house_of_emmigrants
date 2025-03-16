cab13099ba692feb42153b92d6c17f66121ca641
RolandoDEBIAN <rolisrivas@outlook.com>
2025-03-15 22:15:38 -0600

    programe el flask para habilitar navegacion entre pestañas

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from Flask import request
-import pyscopg
+from flask import Flask, render_template, request, redirect, url_for, flash
+# import psycopg
+
+app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    pass
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/changePassword')
+def change_password():
+    return render_template('changePassword.html')
+
+@app.route('/dataExploration')
+def data_exploration():
+    return render_template('dataExploration.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/uploadTool')
+def upload_tool():
+    return render_template('uploadTool.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
